7315156aff0a6aea8b92d7da9e7e9d2f66d46d2d
Dmitry Nikolaev <dmitrii_nikolaev1@epam.com>
2019-06-02 17:13:06 +0300

    Add sprite, add build folder, update build script

--- main.cpp
+++ main.cpp
@@ -2,29 +2,48 @@
 #include <SFML/Graphics.hpp>
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-
 	sf::RenderWindow window(sf::VideoMode(640, 480), "First window!");
 
+	sf::Texture kappaTexture;
+	kappaTexture.loadFromFile("assets/Kappa.png");
 
+	sf::Sprite kappa(kappaTexture);
+
+	sf::Vector2u size = kappaTexture.getSize();
+
+	kappa.setOrigin(size.x / 2, size.y / 2);
+
+	sf::Vector2f increment(0.2f, 0.2f);
+
+	// game loop
 	while (window.isOpen()) {
 		sf::Event event;
 
+		// event loop
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) {
 				window.close();
 			}
+			if (event.key.code == sf::Keyboard::Escape) {
+				window.close();
+			}
 		}
+
+		if ((kappa.getPosition().x + (size.x / 2) > window.getSize().x && increment.x > 0) ||
+			(kappa.getPosition().x - (size.x / 2) < 0 && increment.x < 0)) {
+			increment.x = -increment.x;
+		}
+		if ((kappa.getPosition().y + (size.y / 2) > window.getSize().y && increment.y > 0) ||
+			(kappa.getPosition().y - (size.y / 2) < 0 && increment.y < 0)) {
+			increment.y = -increment.y;
+		}
+		kappa.setPosition(kappa.getPosition() + increment);
 
 		window.clear(sf::Color::Black);
 
-		sf::RectangleShape rectangle(sf::Vector2f(128.0f, 128.0f));
-		rectangle.setFillColor(sf::Color::Red);
-		rectangle.setPosition(320, 240);
-
-		window.draw(rectangle);
-
 		// draw here
+
+		window.draw(kappa);
 
 		window.display();
 	}
